0b29667565df27bce12a719a38860d88c8c69e80
OrderedSet <orderedset86@gmail.com>
2023-04-14 21:30:39 -0700

    Fix page overrun

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,15 +2,18 @@ import Sidebar from "~/components/Sidebar"
 import { HopeProvider } from '@hope-ui/solid'
 import { Box, Grid, GridItem } from '@hope-ui/solid'
 
+const gap = 6;
+const topHeight = 50;
+
 export default function Home() {
   return (
     <main>
       <HopeProvider>
         <Box height="100vh" bg="black" padding={0} margin={0}>
-          <Grid templateRows="repeat(2, 1fr)" gap={6} height="100vh">
+          <Grid templateRows="repeat(2, 1fr)" gap={gap} height="100vh">
             <GridItem h="50px" bg="tomato"/>
-            <GridItem>
-              <Grid templateColumns="repeat(2, 1fr)" gap={6} height="100vh">
+            <GridItem h={window.innerHeight - topHeight - gap}>
+              <Grid templateColumns="repeat(2, 1fr)" gap={gap} height="100%">
                 <GridItem bg="purple"/>
                 <GridItem bg="green">
                   <Sidebar />
