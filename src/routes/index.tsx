3a171ddeea524643caefd243afdfa865743f2817
OrderedSet <orderedset86@gmail.com>
2023-04-14 21:22:31 -0700

    Add top bar

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -7,10 +7,15 @@ export default function Home() {
     <main>
       <HopeProvider>
         <Box height="100vh" bg="black" padding={0} margin={0}>
-          <Grid templateColumns="repeat(2, 1fr)" gap={6} height="100vh">
-            <GridItem bg="tomato"/>
-            <GridItem bg="tomato">
-              <Sidebar />
+          <Grid templateRows="repeat(2, 1fr)" gap={6} height="100vh">
+            <GridItem h="50px" bg="tomato"/>
+            <GridItem>
+              <Grid templateColumns="repeat(2, 1fr)" gap={6} height="100vh">
+                <GridItem bg="purple"/>
+                <GridItem bg="green">
+                  <Sidebar />
+                </GridItem>
+              </Grid>
             </GridItem>
           </Grid>
         </Box>
