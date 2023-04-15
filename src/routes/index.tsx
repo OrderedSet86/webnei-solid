522b9da2293cc6a2e1f0766d5cd18056bb373f03
OrderedSet <orderedset86@gmail.com>
2023-04-14 21:34:15 -0700

    Make window react to resizes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,4 +1,5 @@
 import Sidebar from "~/components/Sidebar"
+import { createSignal, onMount } from 'solid-js';
 import { HopeProvider } from '@hope-ui/solid'
 import { Box, Grid, GridItem } from '@hope-ui/solid'
 
@@ -6,13 +7,26 @@ const gap = 6;
 const topHeight = 50;
 
 export default function Home() {
+  const [rect, setRect] = createSignal({
+    height: window.innerHeight,
+    width: window.innerWidth
+  });
+
+  const handler = (event: Event) => {
+    setRect({ height: window.innerHeight, width: window.innerWidth });
+  };
+
+  onMount(() => {
+    window.addEventListener('resize', handler);
+  });
+
   return (
     <main>
       <HopeProvider>
         <Box height="100vh" bg="black" padding={0} margin={0}>
           <Grid templateRows="repeat(2, 1fr)" gap={gap} height="100vh">
-            <GridItem h="50px" bg="tomato"/>
-            <GridItem h={window.innerHeight - topHeight - gap}>
+            <GridItem h={topHeight} bg="tomato"/>
+            <GridItem h={rect().height - topHeight - gap}>
               <Grid templateColumns="repeat(2, 1fr)" gap={gap} height="100%">
                 <GridItem bg="purple"/>
                 <GridItem bg="green">
