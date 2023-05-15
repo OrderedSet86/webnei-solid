f30805ab7c4897aaec13842f76fd3c8b90dd5271
OrderedSet <orderedset86@gmail.com>
2023-05-14 19:25:27 -0700

    Basic fluid support for GT machines

--- src/components/FallbackRecipeRenderer.tsx
+++ src/components/FallbackRecipeRenderer.tsx
@@ -30,6 +30,7 @@ interface FallbackRecipeRendererProps {
 
 const scaleFactor = 1.25
 const gap = 2
+const recipePadding = 10
 const boxCountToGridSize = (boxSize: number) => {
     return (appState.imageWidth + 2) * scaleFactor * boxSize + gap * (boxSize-1)
 }
@@ -37,20 +38,38 @@ const boxCountToGridSize = (boxSize: number) => {
 const FallbackRecipeRenderer = (props: FallbackRecipeRendererProps) => {
     // Renders a recipe as a grid -> grid (no darkUI asset)
 
-    const heightBoxes = Math.max(props.recipe.dimensions.itemInputDims.height, props.recipe.dimensions.itemOutputDims.height)
-    const gridPxHeight = boxCountToGridSize(heightBoxes)
+    // Compute overall recipe grid attributes
+    const maxInputBoxWidth = Math.max(props.recipe.dimensions.itemInputDims.width, props.recipe.dimensions.fluidInputDims.width)
+    const inputWidth = boxCountToGridSize(maxInputBoxWidth)
 
+    const maxOutputBoxWidth = Math.max(props.recipe.dimensions.itemOutputDims.width, props.recipe.dimensions.fluidOutputDims.width)
+    const outputWidth = boxCountToGridSize(maxOutputBoxWidth)
+
+    const inputHeight = (
+        boxCountToGridSize(props.recipe.dimensions.itemInputDims.height) 
+        + boxCountToGridSize(props.recipe.dimensions.fluidInputDims.height)
+        + gap
+    )
+    const outputHeight = (
+        boxCountToGridSize(props.recipe.dimensions.itemOutputDims.height)
+        + boxCountToGridSize(props.recipe.dimensions.fluidOutputDims.height)
+        + gap
+    )
+    const maxHeight = Math.max(inputHeight, outputHeight)
+
+
+    // Construct single recipe grid
     return (
-        <Box paddingBottom={10}>
+        <Box paddingBottom={recipePadding}>
             <Grid
                 templateColumns={
                     `
-                    ${boxCountToGridSize(props.recipe.dimensions.itemInputDims.width)}px
+                    ${inputWidth}px
                     60px
-                    ${boxCountToGridSize(props.recipe.dimensions.itemOutputDims.width)}px
+                    ${outputWidth}px
                 `}
                 gap={0}
-                height={`${gridPxHeight}px`}
+                height={`${maxHeight}px`}
                 bg={appStyles.recipeBrowserColor}
             >
                 <Center>
@@ -95,14 +114,15 @@ const ItemAndFluidGrid = (props: ItemAndFluidGridProps) => {
         map.set(obj.position, obj);
         return map;
     }, new Map<number, any>());
+    const positionToFluidMapping = props.fluids.reduce((map, obj) => {
+        map.set(obj.position, obj);
+        return map;
+    }, new Map<number, any>());
 
-    const gridWidthLayoutStr = `repeat(${props.itemDims.width}, 1fr)`
-    const gridHeightLayoutStr = `repeat(${props.itemDims.height}, 1fr)`
-
-    return (
+    const itemGrid = (
         <Grid
-            templateColumns={gridWidthLayoutStr}
-            templateRows={gridHeightLayoutStr}
+            templateColumns={`repeat(${props.itemDims.width}, 1fr)`}
+            templateRows={`repeat(${props.itemDims.height}, 1fr)`}
             gap={gap}
             height={boxCountToGridSize(props.itemDims.height)}
             width={boxCountToGridSize(props.itemDims.width)}
@@ -118,7 +138,50 @@ const ItemAndFluidGrid = (props: ItemAndFluidGridProps) => {
                         if (index_obj.hasOwnProperty("stackSize")) {
                             clickableType = "item";
                             quantity = index_obj.stackSize;
-                        } else if (index_obj.hasOwnProperty("liters")) {
+                        } else {
+                            console.log("Unknown clickable type");
+                        }
+
+                        return (
+                            <GridItem bg={appStyles.recipeGridColor}>
+                                <Center>
+                                    <ClickableItem
+                                        tooltipLabel={index_obj.localizedName}
+                                        basic_display_info={index_obj}
+                                        divClass={"cellNoOutline"}
+                                        scaleFactor={scaleFactor}
+                                        advanced_display_info={{quantity: quantity, clickableType: clickableType}}
+                                    />
+                                </Center>
+                            </GridItem>
+                        );
+                    } else return (
+                        // No item at this position
+                        <GridItem
+                            bg={appStyles.recipeGridColor}
+                        />
+                    );
+                }}
+            </Index>
+        </Grid>
+    )
+    const fluidGrid = (
+        <Grid
+            templateColumns={`repeat(${props.fluidDims.width}, 1fr)`}
+            templateRows={`repeat(${props.fluidDims.height}, 1fr)`}
+            gap={gap}
+            height={boxCountToGridSize(props.fluidDims.height)}
+            width={boxCountToGridSize(props.fluidDims.width)}
+        >
+            <Index each={Array.from({ length: props.fluidDims.width * props.fluidDims.height })}>
+                {(_, index) => {
+                    const index_obj = positionToFluidMapping.get(index);
+
+                    if (index_obj) {
+                        let clickableType = "";
+                        let quantity = -1;
+
+                        if (index_obj.hasOwnProperty("liters")) {
                             clickableType = "fluid";
                             quantity = index_obj.liters;
                         } else {
@@ -139,6 +202,7 @@ const ItemAndFluidGrid = (props: ItemAndFluidGridProps) => {
                             </GridItem>
                         );
                     } else return (
+                        // No item at this position
                         <GridItem
                             bg={appStyles.recipeGridColor}
                         />
@@ -146,6 +210,18 @@ const ItemAndFluidGrid = (props: ItemAndFluidGridProps) => {
                 }}
             </Index>
         </Grid>
+    )
+
+    return (
+        <>
+            <Grid
+                templateRows={`${boxCountToGridSize(props.itemDims.height)}px ${boxCountToGridSize(props.fluidDims.height)}px`}
+                gap={gap}
+            >
+                {itemGrid}
+                {fluidGrid}
+            </Grid>
+        </>
     )
 }
 
